1ad5952c5de3c80d5757755fedf45cb98bd85481
doantin135 <132345214+doantin135@users.noreply.github.com>
2025-04-16 17:00:55 +0700

    Add files via upload

--- demo/src/App.js
+++ demo/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello />
+      <Counter />
+     {/*<Message /> */} 
+      {/*<Greet name = "Bruce" heroName = "Batman" >
+        <p>This is children props</p></Greet>
+      <Greet name = "Clark" heroName = "Iroman"> 
+        <button>Action</button>
+      </Greet>
+      <Greet name = "Diana" heroName = "FFrrr"/>
+       <Welcome name = "Bruce" heroName = "Batman"/> 
+       <Welcome name = "Clark" heroName = "Iroman"/> 
+       <Welcome name = "Diana" heroName = "FFrrr"/> 
+       <Hello /> */}
     </div>
   );
 }
--- demo/src/components/Greet.js
+++ demo/src/components/Greet.js
@@ -3,5 +3,17 @@ import React from 'react';
 // function Greet() {
 //   return <h1>Hello doan tin</h1>;
 // }
-const Greet = () => <h1>Hello doan tin</h1>;
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>
+                Hello {props.name} a.k.a {props.heroName}
+            </h1>
+            {props.children}
+        </div>
+   
+)
+    
+}
 export default Greet;
